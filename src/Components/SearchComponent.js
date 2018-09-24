c4336350ce0a7d700e3bd3e8393631a33fd43596
Srikar Kovvali <srikar.kovvali@paytm.com>
2018-09-24 12:26:26 -0400

    Fixed Search Bar Size

--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
@@ -12,14 +11,14 @@ const styles = theme => ({
     flexWrap: 'wrap',
   },
   textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+    marginCenter: theme.spacing.unit,
+    width: 400,
   },
   dense: {
     marginTop: 16,
   },
   menu: {
-    width: 200,
+    width: 400,
   },
 });
 
